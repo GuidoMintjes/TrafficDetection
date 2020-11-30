eb1ed254f1efd37efe8c5dc175b9e8cc6de4e6d1
GuidoMintjes <guidomintjes@leerling.veluwscollege.nl>
2020-11-30 20:07:09 +0100

    Het COCO script laat nu een random plaatje met stoplichten zien, maar ik weet niet zeker of we nog wel verder moeten met COCO...

--- COCO.py
+++ COCO.py
@@ -3,7 +3,11 @@ from pycocotools.coco import COCO
 
 import sys
 
+# Allemaal dingen voor plaatjes
 from cv2 import cv2
+import PIL
+import requests
+import io
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -16,7 +20,21 @@ pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 
 import numpy as np
 import os
-import skimage.io as io
+import skimage.io as skio
+
+
+def loadNShowIm(img, catIds):
+    coco_url = img['coco_url']
+    image_bytes = io.BytesIO(requests.get(coco_url).content)
+    
+    imDecoded = cv2.imdecode(np.frombuffer(image_bytes.read(), np.uint8), 1)
+    
+
+    cv2.imshow('Plaatje', imDecoded)
+    cv2.waitKey()
+
+
+
 
 # Maak nog een check voor eerste keer!
 
@@ -33,19 +51,18 @@ nms = [category['name'] for category in categories]
 nms = set([category['supercategory'] for category in categories])
 
 
+# Bij de categorie traffic light foto's vinden
+print("Alle foto's vinden met stoplichten...")
 catIds = coco.getCatIds(catNms=['traffic light'])
 imgIds = coco.getImgIds(catIds = catIds)
-imgIds = coco.getImgIds(imgIds = [324158])
-img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
 
 
-Image = io.imread(img['coco_url'])
 
-plt.axis('off')
-plt.ioff()
-plt.imshow(Image)
-plt.plot()
+# Een simpele loop
+for i in range(0, 12):
 
-cv2.imshow('Plaatje', Image)
+    print("Laad een willekeurig plaatje...")
+    img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
 
-input('Exit...')
+    print("Laat het willekeurige plaatje zien...")
+    loadNShowIm(img, catIds)
